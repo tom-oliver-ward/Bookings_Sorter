b0f5512d31b89667fff19e5c5857252b408b4ee8
tom-oliver-ward <37127495+tom-oliver-ward@users.noreply.github.com>
2018-05-22 11:16:53 +1200

    Comments added, minor fixes

--- BookingsSorter/BookingsSorter/DataTransfer.cs
+++ BookingsSorter/BookingsSorter/DataTransfer.cs
@@ -9,9 +9,6 @@ namespace BookingsSorter
     class DataTransfer
     {
 
-        //public List<Project> ProjectList=new List<Project>();         //unnecesary?
-        //Project project = new Project(false, null, null, null, null);     //unesccesary?
-
         //initialises instance of class
         HeadingPostitions headingPostitions = new HeadingPostitions(0, 0, 0, 0, 0, 0);
         internal bool add = true;                                        //variable to confirm whether to add the current project or if it already exists
@@ -21,6 +18,7 @@ namespace BookingsSorter
 
         internal bool addU = true;           //variable for whether to add user
         internal int posUser = 1;            //variable for whether to add Equipment
+        //test whether these get reset when it reopens
 
 
         /// <summary>
@@ -63,6 +61,7 @@ namespace BookingsSorter
             //Otherwise proceeds to enter data
             else
             {
+
                 posProject = processing.projectList.Count;
                 //finds if the existing project exists
                 testExistingProject(processing);
@@ -113,52 +112,86 @@ namespace BookingsSorter
 
         }
 
+        /// <summary>
+        /// This creates a new list to add to the base list with the new user listed
+        /// </summary>
+        /// <param name="processing"></param>
         private void addUser(Processing processing)
         {
+            //creates list
             List<string> sublistU = new List<string>();
+            //adds the laser user to the list 
             sublistU.Add(processing.CurrentLine[headingPostitions.LaserUserPosition]);
+            //adds a null point as each sub list will be at least 2 points long
+            sublistU.Add(null);
+            //adds the list to the base list
             processing.projectList[posProject].UseageList.Add(sublistU);
+            //sets the variables back to default
             posUser = 1;
         }
 
+        /// <summary>
+        /// This adds the equipment to the first list  in the base list
+        /// </summary>
+        /// <param name="processing"></param>
         private void addEquipment(Processing processing)
         {
+            //adds the entry
             processing.projectList[posProject].UseageList[0].Add(processing.CurrentLine[headingPostitions.EquipmentPosition]);
-            processing.projectList[posProject].UseageList[0].Add(null);
-            addE = false;
+
+            //sets the variables back to default            
             posEquipment = 1;
         }
 
+        /// <summary>
+        /// Adds the hours at the specified position by posUser and posEquipment
+        /// </summary>
+        /// <param name="processing"></param>
         private void addHours(Processing processing)
         {
             //calculates hours
             float hours = hoursCalc(processing);
 
+            //test if there is already an entry at posUser,posEquipment
             testHourPosExists(processing);
 
-            sumHours(processing,hours);
+            //adds the hours variable to that given point
+            hours = sumHours(processing,hours);
 
+            //sets the given coordinate to the hours output
             processing.projectList[posProject].UseageList[posUser][posEquipment] = Convert.ToString(hours);            
         }
 
-        private void sumHours(Processing processing, float hours)
+        /// <summary>
+        /// Tests if there is a value there or not
+        /// </summary>
+        /// <param name="processing"></param>
+        /// <param name="hours"></param>
+        /// <returns></returns>
+        private float sumHours(Processing processing, float hours)
         {
+            //is entry null?
             if (processing.projectList[posProject].UseageList[posUser][posEquipment] == null)
             {
                 processing.projectList[posProject].UseageList[posUser][posEquipment] = Convert.ToString(hours);
             }
+            //else reads current value and adds the new value
             else
             {
                 float existing = Convert.ToSingle(processing.projectList[posProject].UseageList[posUser][posEquipment]);
                 hours = hours + existing;
                 processing.projectList[posProject].UseageList[posUser][posEquipment] = Convert.ToString(hours);
             }
+            //returns the updated hours variable
+            return hours;
         }
 
-
+        //tests whether the co-ordinate defined by posUser,PosEquipment exists, if not extends required list to that length
         private void testHourPosExists(Processing processing)
         {
+            //if long enough, return
             if (posEquipment <= processing.projectList[posProject].UseageList[posUser].Count-1) { return; }
+            //else add null until it is long enough
             else
             {
                 while (posEquipment <= processing.projectList[posProject].UseageList[posUser].Count - 1)
@@ -222,35 +255,49 @@ namespace BookingsSorter
             addEquipment(processing);
 
             //Adds hours
-            addHours(processing);           
-            
-            add = false;
+            addHours(processing);
+
+            //resets hours
+            posProject = 0;
         }
 
-
-
+        /// <summary>
+        /// Tests if the project already exists
+        /// </summary>
+        /// <param name="processing"></param>
         private void testExistingProject(Processing processing)
         {
+            //iterates through each instance of project
             for (int i = 0; i > processing.projectList.Count; i++)
             {
+                //tests if the current entry matches any of the existing project names
                 if (processing.CurrentLine[headingPostitions.ProjectPosition] == processing.projectList[i].ProjectName)
                 {
+                    //sets add to false, stores position and breaks
                     add = false;
                     posProject = i;
+                    break;
                 }
             }
         }
 
-        private float hoursCalc(Processing formObject)
+        /// <summary>
+        /// Calculates the number of hours for a given bookinh
+        /// </summary>
+        /// <param name="formObject"></param>
+        /// <returns></returns>
+        private float hoursCalc(Processing processing)
         {
-            Processing processing = formObject;
+            //variable positions initialised where xS denotes start, xF denotes finish, and x denotes sum
             int dayS; int dayF; int day;
             int monthS; int monthF; int month;
             int yearS; int yearF; int year;
             int hourS; int hourF; int hour;
             float minuteS; float minuteF; float minute;
+            //total hours
             float hoursTotal;
 
+            //variables customised to the position that the file stores data - this could perhaps be improved to be more automated
             int yearStart = 0;
             int yearLength = 4;
             int monthStart = 5;
@@ -259,12 +306,13 @@ namespace BookingsSorter
             int hourStart = 13;
             int minuteStart = 16;
 
+            //factor, how many hours in a given entry
             int yearFactor = 8760;
             float monthFactor = 730;
             int dayFactor = 24;
             int minuteInvFactor = 60;
 
-
+            //reads length of each entry type
             yearS = Convert.ToInt32(processing.CurrentLine[headingPostitions.StartPosition].Substring(yearStart, yearLength));
             yearF = Convert.ToInt32(processing.CurrentLine[headingPostitions.FinishPosition].Substring(yearStart, yearLength));
             year = yearF - yearS;
@@ -285,8 +333,9 @@ namespace BookingsSorter
             minuteF = Convert.ToInt32(processing.CurrentLine[headingPostitions.FinishPosition].Substring(minuteStart, otherLength));
             minute = minuteF - minuteS;
 
+            //sums total hours from all types
             hoursTotal = year * yearFactor + month * monthFactor + day * dayFactor + hour + minute / minuteInvFactor;
-
+            //returns this
             return hoursTotal;
         }
 
