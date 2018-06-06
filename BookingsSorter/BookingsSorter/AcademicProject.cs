9be8fe8c90d9e9275ff042f6bc777ce44f9c53e4
tom-oliver-ward <37127495+tom-oliver-ward@users.noreply.github.com>
2018-06-06 14:59:05 +1200

    see prev

--- BookingsSorter/BookingsSorter/AcademicProject.cs
+++ BookingsSorter/BookingsSorter/AcademicProject.cs
@@ -8,9 +8,12 @@ namespace BookingsSorter
 {
     public class AcademicProject
     {
+        // Variable to store project name
         public string ProjectName { get; set; }
+        //2D jagged list to store table with user columns and equipment rows, with hours values listed
         public List<List<string>> UseageList = new List<List<string>>();
 
+        //constructor
         public AcademicProject(string projectName, List<List<string>> useageList)
         {
             useageList = UseageList;            
